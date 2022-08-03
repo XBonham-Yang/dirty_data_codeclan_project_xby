d4db828f9c2710de130f6386b2a4e59bbc910e8a
XBonham-Yang <107122564+XBonham-Yang@users.noreply.github.com>
2022-08-03 14:38:12 +0100

    cleaned sweets names

--- task4/data_cleaning_script/task4_cleaning.R
+++ task4/data_cleaning_script/task4_cleaning.R
@@ -35,11 +35,11 @@ candy_17 <- candy_17 %>%
 
 #It looks very messy. and a lot of NA, so I don't know what each col is
 
-candy_17 <- candy_17 %>% 
-  pivot_longer(cols = starts_with("Q12"),
-               names_to = "media",
-               values_to = "ans_of_media")%>%
-  mutate(media = str_sub(media, start = 6))
+#candy_17 <- candy_17 %>% 
+  #pivot_longer(cols = starts_with("Q12"),
+              # names_to = "media",
+               #values_to = "ans_of_media")%>%
+  #mutate(media = str_sub(media, start = 6))
 
 #too many NA here, I don't know what goes in here,
 
@@ -67,42 +67,52 @@ candy_16 <- candy_16 %>%
  select(cols = -c(7:22))
 
 candy_16 %>% 
+  mutate(country = str_to_title(country)) %>% 
   distinct(country) %>% 
   pull()
 #very messy and I will put all the too crazy ones to NA such as 45.0 ...
-list_us <- c("Unites States" ,'United Staes', "Unhinged States",
+list_us <- c("Usa", "Us" ,"United States Of America","United States" ,
+             "Unites States" ,"Ussa","U.s.a." ,
+             'United Staes', "Unhinged States",
              "North Carolina" ,"Unied States" , "U S"  ,"United State" ,
              "New York" ,"Trumpistan"  ,"United Sates"  ,"California"  ,
              "United Stated"  ,"Ahem....Amerca" ,"New Jersey" ,"United Ststes",
              "United Statss" ,"Alaska"  ,"United Statea" ,"United Ststes",
              "Unite States","Pittsburgh","Murica","Murrika","'Merica",
              "The Yoo Ess Of Aaayyyyyy","Sub-Canadian North America... 'Merica",
-             "Merica","United Stetes","Units States")
-list_uk <- c("U.K." ,"Scotland","Endland", "U.k.","United Kindom")
-list_can <-c("Can", "Canae", "Soviet Canuckistan")
+             "Merica","United Stetes","Units States","Usa!",
+             "Usa (I Think But It's An Election Year So Who Can Really Tell)",
+             "U.s.","America","Usa Usa Usa","Usa!!!!!!","Usa! Usa!",
+             "Usa Usa Usa Usa","United  States Of America","United State",
+             "Usa! Usa! Usa!","The Best One - Usa","Usa Usa Usa!!!!","Usausausa",
+             "Us Of A","The United States","The United States Of America",
+             "Usa? Hard To Tell Anymore..","Usas",
+             "I Pretend To Be From Canada, But I Am Really From The United States.",
+             "Usaa", "U S A","Usa Usa Usa!!!!")
+list_uk <- c("Uk" ,"U.K." ,"Scotland","Endland", "U.k.","United Kindom","England",
+             "United Kingdom","United Kingdom","Uk")
+list_can <-c("Can", "Canae", "Soviet Canuckistan","Canada")
 list_NA <- c("Europe","Earth","35","46", "A",  "Insanity Lately","32","1",
              "I Don't Know Anymore","Fear And Loathing", "45", "Narnia",
              "Atlantis","Cascadia","Ud","47.0", "54.0","51.0","God's Country",  
              "One Of The Best Ones","Denial","See Above","30.0",
              "A Tropical Island South Of The Equator","Neverland","This One", 
              "There Isn't One For Old Men","Somewhere","45.0","30.0",
-             "The Republic Of Cascadia","Not The Usa Or Canada", "44.0", "Eua")
+             "The Republic Of Cascadia","Not The Usa Or Canada", "44.0", "Eua",
+             "N. America","Subscribe To Dm4uz3 On Youtube" )
+list_sk <- c("Korea","South Korea")
+list_nl <- c("The Netherlands","Netherlands")
+list_sp <-c("España", "Spain")
 
-candy_16 <- candy_16 %>% 
+candy_16 <-candy_16 %>% 
   mutate(country = str_to_title(country)) %>% 
-  mutate(country = case_when(str_detect(country, '(?i)usa') ~ "US",
-                             str_detect(country, '(?i)america') ~ "US",
-                             str_detect(country, '(?i)us') ~ "US",
-                             str_detect(country, '[Uu ].[Ss].') ~ "US",
-                             str_detect(country, '(?i)united states') ~ "US",
-                             str_detect(country, '(?i)uk') ~"UK",
-                             str_detect(country, '(?i)united kingdom') ~ "UK",
-                             str_detect(country, '(?i)england') ~ "UK",
-                             str_detect(country, '(?i)canada') ~ "Canada",
-                             country %in% list_uk ~ "UK",
-                             country %in% list_us ~"US",
+  mutate(country = case_when(country %in% list_uk ~ "UK",
+                             country %in% list_us ~ "US",
                              country %in% list_can ~"Canada",
                              country %in% list_NA ~ NA_character_,
+                             country %in% list_nl ~ 'Netherlands',
+                             country %in% list_sk ~ "South Korea",
+                             country %in% list_sp ~ "Spain",
                              country =="Uae" ~"UAE",
                              TRUE ~ country))
 
@@ -125,22 +135,18 @@ candy_17 <- candy_17 %>%
 
 candy_17 <- candy_17 %>%
   mutate(country = str_to_title(country)) %>% 
-  mutate(country = case_when(str_detect(country, '(?i)usa') ~ "US",
-                             str_detect(country, '(?i)america') ~ "US",
-                             str_detect(country, '(?i)us') ~ "US",
-                             str_detect(country, '[Uu ].[Ss].') ~ "US",
-                             str_detect(country, '(?i)united states') ~ "US",
-                             str_detect(country, '(?i)uk') ~"UK",
-                             str_detect(country, '(?i)united kingdom') ~ "UK",
-                             str_detect(country, '(?i)england') ~ "UK",
-                             str_detect(country, '(?i)canada') ~ "Canada",
-                             country %in% list_uk ~ "UK",
+  mutate(country = case_when(country %in% list_uk ~ "UK",
                              country %in% list_us ~"US",
                              country %in% list_can ~"Canada",
                              country %in% list_NA ~ NA_character_,
+                             country %in% list_nl ~ 'Netherlands',
+                             country %in% list_sk ~ "South Korea",
+                             country %in% list_sp ~ "Spain",
                              country =="Uae" ~"UAE",
                              TRUE ~ country))
- 
+candy_17 %>% 
+  distinct(country) %>% 
+  pull()
 #I only kept the col that are useful as the data frame is too big 
 #I am only keeping the years in time stamp 
 candy_15_s <- candy_15 %>% 
@@ -148,27 +154,130 @@ candy_15_s <- candy_15 %>%
   mutate(age = as.integer(age))
 candy_16_s <- candy_16 %>% 
   mutate(Timestamp = "2016") %>% 
-  mutate(age = as.integer(age))
+  mutate(age = as.integer(age)) %>% 
+  select(-6)
 candy_17_s <- candy_17 %>% 
   mutate(Timestamp = "2017") %>% 
-  mutate(age = as.integer(age))
+  mutate(age = as.integer(age)) %>% 
+  select(c(2,3,4,5,11,12,13))
+
+#checks on each columns--age
+candy_15_s <- candy_15_s %>% 
+  mutate(age = case_when(age >= 120 ~ NA_integer_,
+                         age <= 0 ~ NA_integer_,
+                         TRUE ~ age))
+
+
+candy_16_s <- candy_16_s %>% 
+  mutate(age = case_when(age >= 120 ~ NA_integer_,
+                         age <= 0 ~ NA_integer_,
+                         TRUE ~ age))
+
+candy_17_s <- candy_17_s %>% 
+  mutate(age = case_when(age >= 120 ~ NA_integer_,
+                         age <= 0 ~ NA_integer_,
+                         TRUE ~ age))
+
+candy_17_s %>% 
+  select(age) %>% 
+  distinct() %>% 
+  pull()
+#check on going_out
+candy_17_s %>% 
+  select(going_out) %>% 
+  distinct() %>% 
+  pull()
+#check on emotions 
+candy_17_s %>% 
+  select(emotion) %>% 
+  distinct() %>% 
+  pull()
+#check on gender
+candy_17_s %>% 
+  select(gender) %>% 
+  distinct() %>% 
+  pull()
+#other and I'd rather not say also allowed 
+candy_17_s %>% 
+  select(sweets) %>% 
+  distinct() %>% 
+  pull()
+#2015_95 sweets
+#2016-101 sweets
+#2017_103 sweets 
+#didn't realise that this is dirty too.
+#I am not very familiar with candy names, this might not be
+#100% correct
+candy_na <- c("Cash, or other forms of legal tender","Generic Brand Acetaminophen",
+              "Glow sticks", "Broken glow stick",
+              "Creepy Religious comics/Chick Tracts",                                       
+              "Healthy Fruit","Hugs (actual physical hugs)", "Kale smoothie",
+              "Minibags of chips", "JoyJoy (Mit Iodine)", "Pencils", 
+              "Peterson Brand Sidewalk Chalk",  "Vicodin", "White Bread", 
+              "Whole Wheat anything",  
+              "Person of Interest Season 3 DVD Box Set (not including Disc 4 with hilarious outtakes)",
+              "Vials of pure high fructose corn syrup, for main-lining into your vein",
+              "York Peppermint Patties] Ignor","Chardonnay", "Generic Brand Acetaminophen",
+              "Abstained from M&M'ing.","Real Housewives of Orange County Season 9 Blue-Ray",
+              "Spotted Dick", "Bonkers (the board game)" ,"Sea-salt flavored stuff, probably chocolate, since this is the \"it\" flavor of the year"
+              )
+candy_mm <- c("Regular M&Ms","Blue M&M's", "Red M&M's", "Green Party M&M's",
+              "Independent M&M's" )
+
+
+candy_mj <- c("Mary Janes","Anonymous brown globs that come in black and orange wrappers",
+              "Anonymous brown globs that come in black and orange wrappers\t(a.k.a. Mary Janes)")
+
+candy_l <- c("Licorice", "Licorice (yes black)")
+candy_b <- c("Bonkers", "Bonkers (the candy)" )
+candy_st <- c("Sweetums (a friend to diabetes)","Sweetums" )
+
+candy_15_s <- candy_15_s %>%
+  mutate(sweets = case_when(sweets %in% candy_na ~ NA_character_,
+                             sweets %in% candy_mm ~"Regular M&M",
+                             sweets %in% candy_mj ~ "Mary Janes",
+                            sweets %in% candy_l ~ "Licorice",
+                            sweets %in% candy_b ~ "Bonkers",
+                            sweets %in% candy_st ~ 'Sweetums',
+                             TRUE ~ sweets)) %>% 
+  arrange(sweets)
+
+candy_16_s <- candy_16_s %>%
+  mutate(sweets = case_when(sweets %in% candy_na ~ NA_character_,
+                            sweets %in% candy_mm ~"Regular M&M",
+                            sweets %in% candy_mj ~ "Mary Janes",
+                            sweets %in% candy_l ~ "Licorice",
+                            sweets %in% candy_b ~ "Bonkers",
+                            sweets %in% candy_st ~ 'Sweetums',
+                            TRUE ~ sweets)) 
+
+
+candy_17_s <- candy_17_s %>%
+  mutate(sweets = case_when(sweets %in% candy_na ~ NA_character_,
+                            sweets %in% candy_mm ~"Regular M&M",
+                            sweets %in% candy_mj ~ "Mary Janes",
+                            sweets %in% candy_l ~ "Licorice",
+                            sweets %in% candy_b ~ "Bonkers",
+                            sweets %in% candy_st ~ 'Sweetums',
+                            TRUE ~ sweets)) 
+candy_15_s %>% 
+  select(sweets) %>% 
+  distinct() %>% 
+  pull()
+
 
 candy_all <- candy_15_s %>% 
   full_join(candy_16_s) %>% 
-  full_join(candy_17_s) %>% janitor::clean_names()
+  full_join(candy_17_s) %>%
+  arrange(sweets) %>% 
+  janitor::clean_names()
 
-
-candy_all <- candy_all %>%
- mutate(age = case_when(age >= 120 ~ NA_integer_,
-                        age <= 0 ~ NA_integer_,
-                        TRUE ~ age)) %>% 
-  select(1:7)
   
-  
-#I went back from analysis as my age was messy. 
-#I will treat age over 120 and 0 as NA.
 candy_all %>% 
-  distinct(age) %>% 
+  select(sweets) %>% 
+  distinct() %>% 
   pull()
 
+
 write_csv(candy_all, "clean_data/clean_candy")
+
